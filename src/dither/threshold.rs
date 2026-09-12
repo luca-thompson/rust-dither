832e3081c92f2942964be9357a1b16df326db591
luca-thompson <lucathompson006@gmail.com>
2026-09-12 18:18:06 +1000

    io test finished

--- src/dither/threshold.rs
+++ src/dither/threshold.rs
@@ -4,8 +4,8 @@ pub fn threshold_dither(image: &mut PixelBuffer){
 
     let (width, height) = image.get_dimensions();
 
-    for x in 0..width{
-        for y in 0..height{
+    for x in 0..width {
+        for y in 0..height {
             if image.get_pixel(x, y) < 128.0 {
                 image.put_pixel(x, y, 0.0);
             }
--- tests/io.rs
+++ tests/io.rs
@@ -1,4 +1,21 @@
+#[path = "../src/pixel_buffer.rs"] mod pixel_buffer;
+use pixel_buffer::PixelBuffer;
+
+//image
+use image::ImageReader;
+
+
 #[test]
-fn test_add() {
-    assert_eq!(adder::add(3, 2), 5);
+
+fn test_io() {
+    let test_image_vec: Vec<f32> = vec![0.0, 255.0, 255.0, 0.0];
+    let test_pixel_buffer: PixelBuffer = PixelBuffer{width: 2, height: 2, pixels: test_image_vec};
+
+    let image = ImageReader::open("tests/test_image_bw_2x2.png").unwrap().decode().unwrap();
+    
+    let real_pixel_buffer: PixelBuffer = PixelBuffer::to_pixel_buffer(&image);
+
+    assert_eq!(test_pixel_buffer.width, real_pixel_buffer.width);
+    assert_eq!(test_pixel_buffer.height, real_pixel_buffer.height);
+    assert_eq!(test_pixel_buffer.pixels, real_pixel_buffer.pixels);
 }
